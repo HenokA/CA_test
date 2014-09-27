cbeb6ae5ac936117f62fb601a89b87d919bbd729
Henok <henok@Hannas-MacBook-Pro.local>
2014-09-27 19:39:26 -0400

    Got some style in and basic graphing to work

--- catest.rb
+++ catest.rb
@@ -8,11 +8,25 @@ get '/' do
 	erb :"index.html"
 end	
 
+get '/home' do
+	erb :"home.html"
+end
+
+# post '/home' do
+#   session['state'] = params[:state]
+#   session['city'] = params[:city]
+#   session['b1'] = params[:button1]
+
+#   redirect '/schools'
+# end
+
 get '/about' do
 	erb :"about.html"
 end
 
 get '/schools' do
-	erb :"schools.html"
+	location = session['state'].to_s
+	puts "jajkalksdfjlaksjdflkajsdlkfjalskjflaskjdf" + session['b1'].to_s
+	erb :"schools.html", :locals => {:location => location}
 end
 
