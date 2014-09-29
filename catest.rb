6e4b092dc9e73b4eac800dccaadcdfe86c5aa214
Henok <henok@Hannas-MacBook-Pro.local>
2014-09-29 04:41:38 -0400

    Displaying basic information for san francisco district school students. Need to add better select and, styling, and a better home page

--- catest.rb
+++ catest.rb
@@ -24,19 +24,47 @@ get '/districts' do
 end
 
 post '/home' do
-  session['state'] = params[:state]
-  session['city'] = params[:city]
+  session['school'] = params[:school]
+  session['test'] = params[:test]
+  session['grade']= params[:grade]
   session['b1'] = params[:button1]
-
-  redirect '/schools'
+  redirect('/schools/'+params[:school]+"/"+params[:grade]+"/"+params[:test]);
 end
 
 get '/about' do
 	erb :"about.html"
 end
 
-get '/schools' do
-	location = session['state'].to_s
-	erb :"schools.html", :locals => {:location => location}
+get '/schools/:school/:grade/:test' do
+	mean = Array.new;
+	first = Array.new;
+	second = Array.new;
+	third = Array.new;
+	fourth = Array.new;
+	fifth = Array.new;
+	sixth = Array.new;
+	  File.open('sf.txt', 'r'){ |file|
+		file.each_line do |line|
+			line = line.split(',')
+			if (params[:school] == line[2] && params[:test] == line[12] && params[:grade] == line[11])
+				mean.push(line[16].to_s)
+				first.push(line[17].to_s)
+				second.push(line[18].to_s)
+				third.push(line[19].to_s)
+				fourth.push(line[20].to_s)
+				fifth.push(line[21].to_s)
+				sixth.push(line[22].to_s)
+			end
+		end
+	}
+	if mean.empty?
+		redirect('/nopage')
+	end
+	erb :"schools.html", :locals => {:mean => mean, :first => first, :second => second, :third => third, :fourth => fourth,
+										:fifth => fifth, :sixth => sixth}
+end
+
+get '/nopage' do
+	erb :"fail.html"
 end
 
--- parse.rb
+++ parse.rb
@@ -1,12 +1,12 @@
-File.open('2010_clean.txt', 'a'){ |save|
+File.open('2007_clean.txt', 'a'){ |save|
 
-	File.open('2010.txt', 'r'){ |file|
+	File.open('2007.txt', 'r'){ |file|
 		file.each_line do |line|
 			line = line.split(',')
 			if ((line[16] != '"*"' && line[16] != '""') && 
 					(line[17] != '"*"' && line[17] != '""') && (line[18] != '"*"' && line[18] != '""') &&
 					(line[19] != '"*"' && line[19] != '""') && (line[20] != '"*"' && line[20] != '""') &&
-					(line[21] != '"*"' && line[21] != '""') && (line[22] != '"*"' && line[22] != '""'))
+					(line[21] != '"*"' && line[21] != '""') && (line[22] != '"*"' && line[22] != '""') && line[0] == '"38"')
 				i = 0;
 				while i < 23 do
 					if line[i][0] == '"'
@@ -20,3 +20,18 @@ File.open('2010_clean.txt', 'a'){ |save|
 		end
 	}
 }
+
+# school_codes = Array.new
+# File.open('2010_clean.txt', 'r'){ |file|
+# 	file.each_line do |line|
+# 		line = line.split(',')
+# 		if ! (school_codes.include? line[2])
+# 			school_codes.push(line[2].to_s)
+# 		end
+# 	end
+# }
+# File.open('school_codes.txt', 'w'){ |file|
+# 	school_codes.each do |code|
+# 		file.write("<option> " + code+"</option>\n")
+# 	end
+# }
